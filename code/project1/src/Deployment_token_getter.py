f561f6cf6c30e97a9b032c813ea38904e493ecd6
some <some@some.some>
2022-02-10 20:32:18 +0100

    Included argument parser.

---
+++ code/project1/src/Deployment_token_getter.py
@@ -0,0 +1,85 @@
+# Code that automatically copies all issues of a repository to another
+from .control_website import login
+from .Hardcoded import Hardcoded
+from .helper import get_labels_from_issues
+from .get_data import get_value_from_html_source
+from .get_gitlab_runner_token import get_runner_registration_token_from_page
+from .get_website_controller import get_website_controller
+from .helper import get_browser_drivers
+from .helper import parse_creds
+from .helper import click_element_by_xpath
+from .helper import get_runner_registration_token_filepath
+from .helper import source_contains
+from .helper import write_string_to_file
+from .helper import loiter_till_gitlab_server_is_ready_for_login
+from .control_website import open_url
+from .Website_controller import Website_controller
+from .get_data import get_issues
+from .set_data import set_labels
+from .set_data import set_issues
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import time
+
+
+class Deployment_token_getter:
+    """ """
+
+    def __init__(self, project_nr, should_login=True):
+        """Initialises object that gets the browser controller, then it gets the issues
+        from the source repo, and copies them to the target repo.
+
+        :param project_nr: [Int] that indicates the folder in which this code is stored.
+        :param login: [Boolean] True if the website_controller object should be
+        created and should login to GitHub.
+        """
+
+        # project_nr is an artifact of folder structure
+        self.project_nr = project_nr
+        self.relative_src_filepath = f"code/project{self.project_nr}/src/"
+        # Store the hardcoded values used within this project
+        self.hc = Hardcoded()
+
+        # get browser drivers
+        get_browser_drivers(self.hc)
+
+        website_controller = get_website_controller(self.hc)
+
+        # Create for loop that checks if GitLab server page is loaded and ready for login.
+        # loop it for 900 seconds, check page source every 5 seconds
+        loiter_till_gitlab_server_is_ready_for_login(
+            self.hc, 1200, 5, website_controller
+        )
+
+        # Log into GitLab server.
+        website_controller = login(self.hc)
+
+        # wait five seconds for page to load
+        time.sleep(5)
+
+        runner_registration_token = get_runner_registration_token_from_page(
+            website_controller
+        )
+
+        # Export runner registration token to file
+        if len(runner_registration_token) > 14:
+            write_string_to_file(
+                runner_registration_token, get_runner_registration_token_filepath()
+            )
+        else:
+            raise Exception(
+                "Expected runner registration token to be EXTRACTED from the source code, but it is not."
+            )
+
+        # close website controller
+        website_controller.driver.close()
+
+        print(
+            f"Got the GitLab runner registration token, can now proceed with setting up the GitLab CI."
+        )
+
+
+if __name__ == "__main__":
+    # initialize main class
+    main = Main()
--- code/project1/src/__main__.py
+++ code/project1/src/__main__.py
@@ -1,10 +1,40 @@
+import argparse
+
 from .Main import Main
+from .Deployment_token_getter import Deployment_token_getter
 
 print(
     f"Hi, I'll ask you from which source repo to which target repo you want to copy the issues, then I'll download browser controllers and ensure the firefox browser is installed. Next I will scrape the issues from the source repo, and add them as new issues to the target repo. You can simply see what I do in the browser. Terminate me with CTRL+C if you don't like it. I'll let you know when I'm done."
 )
 project_nr = 1
 
-main = Main(project_nr)
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--g",
+    dest="gitlab_runner",
+    action="store_true",
+    help="boolean flag, determines whether the code gets the GitLab Runner token or not.",
+)
+parser.add_argument(
+    "--d",
+    dest="deploy_token",
+    action="store_true",
+    help="boolean flag, determines whether the code gets the deploy token or not",
+)
+parser.set_defaults(
+    gitlab_runner=True,
+    deploy_token=False,
+)
+args = parser.parse_args()
+if args.deploy_token:
+    print(f"Getting GitHub deploy token.")
+    args.gitlab_runner=False
+    deployment_token_getter = Deployment_token_getter(project_nr)
+elif args.gitlab_runner:
+    print(f"Getting GitLab runner token.")
+    args.gitlab_runner=False
+    main = Main(project_nr)
+
+
 
 print(f"Done.")
